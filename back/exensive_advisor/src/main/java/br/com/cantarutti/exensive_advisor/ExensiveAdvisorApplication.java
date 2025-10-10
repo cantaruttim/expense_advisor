d0f8c15de3bbcc1fbbc81b6236762534d5c466a1
MATHEUS DE ALMEIDA CANTARUTTI <matheus.acantarutti@senacsp.edu.br>
2025-10-10 20:45:22 -0300

    backend added

---
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/exensive_advisor/ExensiveAdvisorApplication.java
@@ -0,0 +1,13 @@
+package br.com.cantarutti.exensive_advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExensiveAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExensiveAdvisorApplication.class, args);
+	}
+
+}
---
+++ back/exensive_advisor/src/test/java/br/com/cantarutti/exensive_advisor/ExensiveAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cantarutti.exensive_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExensiveAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
