e31c795b65c94a07ca80db4c14b153eef0737890
cantaruttim <cantaruttim@outlook.com>
2025-10-10 21:48:35 -0300

    repository and jpa dependecy

--- back/exensive_advisor/src/main/java/br/com/cantarutti/data/dto/ExpensesDTO.java
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/data/dto/ExpensesDTO.java
@@ -1,5 +1,18 @@
 package br.com.cantarutti.data.dto;
 
+import org.springframework.data.annotation.Id;
+
+
 public class ExpensesDTO {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String owner;
+    private String card ;
+    private String anomes;
+    private Double value;
+
+
 
 }
