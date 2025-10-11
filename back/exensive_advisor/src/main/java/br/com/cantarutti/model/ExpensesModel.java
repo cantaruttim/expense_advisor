a3b8c35dbefd4a26a5533d21ef43c84b2f647583
cantaruttim <cantaruttim@outlook.com>
2025-10-10 21:08:44 -0300

    implemented Expenses Model

--- back/exensive_advisor/src/main/java/br/com/cantarutti/model/ExpensesModel.java
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/model/ExpensesModel.java
@@ -1,7 +1,55 @@
 package br.com.cantarutti.model;
 
+import java.util.Objects;
+
 public class ExpensesModel {
 
+    private String owner;
+    private String card ;
+    private String anomes;
+    private Double value;
 
+    public String getOwner() {
+        return owner;
+    }
 
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getAnomes() {
+        return anomes;
+    }
+
+    public void setAnomes(String anomes) {
+        this.anomes = anomes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ExpensesModel that)) return false;
+        return Objects.equals(owner, that.owner) && Objects.equals(card, that.card) && Objects.equals(anomes, that.anomes) && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(owner, card, anomes, value);
+    }
 }
