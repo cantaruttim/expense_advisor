84f4b9f72803affe8f84a4592458655ed8bc3d08
cantaruttim <cantaruttim@outlook.com>
2025-10-10 21:56:31 -0300

    updated

--- back/exensive_advisor/src/main/java/br/com/cantarutti/service/ExpensesService.java
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/service/ExpensesService.java
@@ -1,4 +1,13 @@
 //package br.com.cantarutti.service;
 //
+//
+//import br.com.cantarutti.repository.ExpensesRepository;
+//import org.springframework.stereotype.Service;
+//
+//@Service
 //public class ExpensesService {
+//
+//    private final ExpensesRepository repository;
+//
+//
 //}
