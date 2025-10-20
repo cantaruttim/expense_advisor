275265c925b82b86a2bf7f69d7f84d4900087914
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-10-20 18:33:16 -0300

    Uncomment @Repository annotation in GenericExpenseRepository
    Uncommented the @Repository annotation for the interface.

--- back/exensive_advisor/src/main/java/br/com/cantarutti/repository/GenericExpenseRepository.java
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/repository/GenericExpenseRepository.java
@@ -1,4 +1,7 @@
+package br.com.cantarutti.repository;
+import org.springframework.stereotype.Repository;
 
+// @Repository
 public interface ExpensesDataRepository {
     void save(List<Map<String, Object>> data);
 }
