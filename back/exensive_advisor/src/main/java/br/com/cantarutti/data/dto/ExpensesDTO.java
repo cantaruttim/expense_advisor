65e604d65a505fe4c9614a746c3bcd0156bf24df
cantaruttim <cantaruttim@outlook.com>
2025-10-19 21:08:56 -0300

    converting model to dto

--- back/exensive_advisor/src/main/java/br/com/cantarutti/data/dto/ExpensesDTO.java
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/data/dto/ExpensesDTO.java
@@ -5,8 +5,6 @@ import org.springframework.data.annotation.Id;
 
 public class ExpensesDTO {
 
-    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String owner;
     private String card ;
@@ -15,4 +13,43 @@ public class ExpensesDTO {
 
     public ExpensesDTO() {}
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public String getAnomes() {
+        return anomes;
+    }
+
+    public void setAnomes(String anomes) {
+        this.anomes = anomes;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
 }
--- back/exensive_advisor/src/main/java/br/com/cantarutti/mapper/ObjectExpensesMapper.java
+++ back/exensive_advisor/src/main/java/br/com/cantarutti/mapper/ObjectExpensesMapper.java
@@ -1,5 +1,19 @@
 package br.com.cantarutti.mapper;
 
+import br.com.cantarutti.data.dto.ExpensesDTO;
+import br.com.cantarutti.model.ExpensesModel;
+
 public class ObjectExpensesMapper {
 
+    // expenses model to expenses dto
+    public static ExpensesDTO ExpensesToDTO(ExpensesModel model) {
+
+        ExpensesDTO dto = new ExpensesDTO();
+        dto.setId(model.getId());
+        dto.setOwner(model.getOwner());
+        dto.setCard(model.getCard());
+        dto.setAnomes(model.getAnomes());
+        dto.setValue(model.getValue());
+        return dto;
+    }
 }
