e6a415e97cce01188d4c34ea53e62531c42eac1c
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-10-20 18:41:31 -0300

    Delete back/exensive_advisor/src/main/java/br/com/cantarutti/repository/GenericExpenseRepository.java

--- back/exensive_advisor/src/main/java/br/com/cantarutti/repository/GenericExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.cantarutti.repository;
-import org.springframework.stereotype.Repository;
-
-// @Repository
-public interface ExpensesDataRepository {
-    void save(List<Map<String, Object>> data);
-}
