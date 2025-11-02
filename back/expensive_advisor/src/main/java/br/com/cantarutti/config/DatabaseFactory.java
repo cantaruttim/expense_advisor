78e04a2e4774241dcf5779b47f32084fb0ede28e
cantaruttim <cantaruttim@outlook.com>
2025-11-02 11:56:51 -0300

    new config classes

--- back/expensive_advisor/src/main/java/br/com/cantarutti/config/DatabaseFactory.java
+++ back/expensive_advisor/src/main/java/br/com/cantarutti/config/DatabaseFactory.java
@@ -1,4 +1,38 @@
 package br.com.cantarutti.config;
 
+import br.com.cantarutti.model.finance_data.dbs.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DatabaseFactory {
+
+    @Bean
+    public DatabaseManager databaseManager(DatabaseProperties props) {
+        String active = props.getActive().toLowerCase();
+
+        return switch (active) {
+            case "mongodb" -> new MongoDBManager(
+                    props.getMongodb().getUri(),
+                    props.getMongodb().getDatabase(),
+                    props.getMongodb().getCollection()
+            );
+
+            case "postgresql" -> new PostgresDBManager(
+                    props.getPostgresql().getUrl(),
+                    props.getPostgresql().getUser(),
+                    props.getPostgresql().getPassword(),
+                    props.getPostgresql().getTable()
+            );
+
+            case "mysql" -> new MySQLDBManager(
+                    props.getMysql().getUrl(),
+                    props.getMysql().getUser(),
+                    props.getMysql().getPassword(),
+                    props.getMysql().getTable()
+            );
+
+            default -> throw new IllegalArgumentException("Database not supported: " + active);
+        };
+    }
 }
--- back/expensive_advisor/src/main/java/br/com/cantarutti/config/DatabaseProperties.java
+++ back/expensive_advisor/src/main/java/br/com/cantarutti/config/DatabaseProperties.java
@@ -1,4 +1,81 @@
 package br.com.cantarutti.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app.database")
 public class DatabaseProperties {
+    private String active;
+    private Mongo mongodb;
+    private Postgres postgresql;
+    private Mysql mysql;
+
+    public String getActive() { return active; }
+    public void setActive(String active) { this.active = active; }
+
+    public Mongo getMongodb() { return mongodb; }
+    public void setMongodb(Mongo mongodb) { this.mongodb = mongodb; }
+
+    public Postgres getPostgresql() { return postgresql; }
+    public void setPostgresql(Postgres postgresql) { this.postgresql = postgresql; }
+
+    public Mysql getMysql() { return mysql; }
+    public void setMysql(Mysql mysql) { this.mysql = mysql; }
+
+    // ---- MONGO ----
+    public static class Mongo {
+        private String uri;
+        private String database;
+        private String collection;
+
+        public String getUri() { return uri; }
+        public void setUri(String uri) { this.uri = uri; }
+
+        public String getDatabase() { return database; }
+        public void setDatabase(String database) { this.database = database; }
+
+        public String getCollection() { return collection; }
+        public void setCollection(String collection) { this.collection = collection; }
+    }
+
+    // ---- POSTGRES ----
+    public static class Postgres {
+        private String url;
+        private String user;
+        private String password;
+        private String table;
+
+        public String getUrl() { return url; }
+        public void setUrl(String url) { this.url = url; }
+
+        public String getUser() { return user; }
+        public void setUser(String user) { this.user = user; }
+
+        public String getPassword() { return password; }
+        public void setPassword(String password) { this.password = password; }
+
+        public String getTable() { return table; }
+        public void setTable(String table) { this.table = table; }
+    }
+
+    // ---- MYSQL ----
+    public static class Mysql {
+        private String url;
+        private String user;
+        private String password;
+        private String table;
+
+        public String getUrl() { return url; }
+        public void setUrl(String url) { this.url = url; }
+
+        public String getUser() { return user; }
+        public void setUser(String user) { this.user = user; }
+
+        public String getPassword() { return password; }
+        public void setPassword(String password) { this.password = password; }
+
+        public String getTable() { return table; }
+        public void setTable(String table) { this.table = table; }
+    }
 }
